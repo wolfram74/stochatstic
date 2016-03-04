0c545add2a4140a2c553f1b19f10c9b600bfe158
wolfram74 <phaugen@gmail.com>
2016-03-04 15:34:20 -0600

    basic shout bot up and running

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,32 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$('document').ready(function(){
+  console.log('loaded')
+  $('.chat-input').on('submit', 'form', chatHandler)
+})
+
+var chatHandler = function(event){
+  event.preventDefault()
+  var $target = $(event.target)
+  var data = $target.serializeArray()
+  chatUpdate('You', {text:data[0].value})
+  $target.find('input[type=text]').val('')
+  var call = $.ajax({
+    url: './message',
+    type: 'post',
+    data: data
+  })
+  call.done(responseHandler)
+}
+
+var responseHandler = function(response){
+  console.log('things happened!')
+  console.log(response)
+  chatUpdate('SHOUT-BOT', response)
+}
+
+var chatUpdate = function(name, content){
+  console.log(content)
+  var $line = $('<div></div>')
+  $line.attr('class', name)
+  $line.text(name +' : '+content.text)
+  $('.chat-log').append($line)
+}
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,9 @@ class WelcomeController < ApplicationController
   def index
   end
 
+  def message
+    p params
+    reply = params['text'].upcase + '?'
+    render json: {status: 200, text: reply}
+  end
 end
