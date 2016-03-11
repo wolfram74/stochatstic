e095549e858fd6853892b909ef296b7e5e9e3997
wolfram74 <phaugen@gmail.com>
2016-03-11 13:33:59 -0600

    set up chatbot service delegation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
   def message
     p params
     reply = params['text'].upcase + '?'
+    ChatInterface.shout(params['text'])
     render json: {status: 200, text: reply}
   end
 end
---
+++ app/services/chat_interface.rb
@@ -0,0 +1,5 @@
+class ChatInterface
+  def self.shout (string)
+    p [string, 'bot']
+  end
+end
